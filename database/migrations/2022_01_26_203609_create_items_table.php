67b0dc99f6cfc17b3d1e689dc0f7c00a2a244b89
Abraham Peter <theclassical001@gmail.com>
2022-01-26 21:41:46 +0100

    initial commit with item model -m

---
+++ database/migrations/2022_01_26_203609_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->String('name');
+            $table->boolean('complete')->default(false);
+            $table->timestamp('complete_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
